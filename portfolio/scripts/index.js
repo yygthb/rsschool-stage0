93f7c477aff0a6937f096cb4e0774181074e2e9a
Yura Ya <yura.ya.dev@gmail.com>
2022-01-05 13:42:30 +0300

    feat: slow scroll to section after nav-item click

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -1,1 +1,20 @@
-console.log('js');
+const header = document.querySelector('.header');
+const navLinks = header.querySelectorAll('a[href*="#"]');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const sectionId = link.getAttribute('href').substr(1);
+
+    const yOffset = -header.offsetHeight;
+    const element = document.querySelector(`#${sectionId}`);
+    const y =
+      element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth',
+    });
+  });
+});
