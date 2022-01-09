495a92204daa1da76b4c0144ee9a918397cea4b6
Yura Ya <yura.ya.dev@gmail.com>
2022-01-09 11:54:34 +0300

    refactor: update scroll-behavior

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -1,25 +1,3 @@
-// scroll to section title
-const header = document.querySelector('.header');
-const navLinks = header.querySelectorAll('a[href*="#"]');
-
-navLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const sectionId = link.getAttribute('href').substr(1);
-
-    const yOffset = -header.offsetHeight;
-    const element = document.querySelector(`#${sectionId}`);
-    const y =
-      element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
-    window.scrollTo({
-      top: y,
-      behavior: 'smooth',
-    });
-  });
-});
-
 // form
 const form = document.querySelector('.contacts-form');
 const actionButton = form.querySelector('.button');
