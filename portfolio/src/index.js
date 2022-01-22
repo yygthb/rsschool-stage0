0d89d0205a41f446653c11b3a86401c749857de9
Yura Ya <yura.ya.dev@gmail.com>
2022-01-22 22:30:38 +0300

    feat: add light theme

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 
 import './scripts/mobile-menu.js';
+import './scripts/theme.js';
 import './scripts/translate.js';
 import './scripts/form.js';
 import './scripts/portfolio.js';
