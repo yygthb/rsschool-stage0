039bd154fba65e236149336cce12e5f579e7c421
Yura Ya <yura.ya.dev@gmail.com>
2022-01-28 12:28:41 +0300

    feat: responsive video player with volume control

--- portfolio/src/scripts/video.js
+++ portfolio/src/scripts/video.js
@@ -1,19 +1,26 @@
-const vidWrap = document.querySelector('.player');
-const video = vidWrap.querySelector('.player__video');
+const videoContainer = document.querySelector('.player');
+const video = videoContainer.querySelector('.player__video');
+video.volume = 0.5;
 
 // controls
-const mainBtn = vidWrap.querySelector('.play-button');
-const playerBtn = vidWrap.querySelector('.player__button');
-const progress = vidWrap.querySelector('.player__progress');
-const progressBar = vidWrap.querySelector('.progress__filled');
+const mainBtn = videoContainer.querySelector('.button-play__main');
+const playerBtn = videoContainer.querySelector('.button-play');
+const volBtn = videoContainer.querySelector('.button-volume');
+const progress = videoContainer.querySelector('.player__progress');
+const progressBar = videoContainer.querySelector('.progress__filled');
+const volume = videoContainer.querySelector('.player__volume');
+const volumeBar = videoContainer.querySelector('.volume__filled');
+const volumeThumb = videoContainer.querySelector('.volume__thumb');
 
 // events
 let drag;
 let grap;
 
 //
+video.addEventListener('ended', stopVideo);
 mainBtn.addEventListener('click', startVideo);
 playerBtn.addEventListener('click', toggleVideo);
+volBtn.addEventListener('click', toggleVolume);
 progress.addEventListener('mouseover', function () {
   drag = true;
 });
@@ -33,18 +40,22 @@ progress.addEventListener('mousemove', function (e) {
     updateCurrentPos(e);
   }
 });
+volume.addEventListener('change', (e) => {
+  updateVol(e.target.value);
+});
 
 let progression;
+let volumeSaved = 50;
 
 function startVideo() {
-  vidWrap.classList.add('active');
+  videoContainer.classList.add('active');
   video.play();
   updateProgress();
   progression = window.setInterval(updateProgress, 200);
 }
 
 function stopVideo() {
-  vidWrap.classList.remove('active');
+  videoContainer.classList.remove('active');
   video.pause();
   clearInterval(progression);
 }
@@ -57,16 +68,35 @@ function toggleVideo() {
   }
 }
 
+function toggleVolume() {
+  if (video.volume > 0) {
+    volumeSaved = video.volume;
+    updateVol(0);
+  } else {
+    updateVol(volumeSaved);
+  }
+}
+
 function updateProgress() {
-  var progress = video.currentTime / video.duration;
+  const progress = video.currentTime / video.duration;
   progressBar.style.flexBasis = Math.floor(progress * 1000) / 10 + '%';
 }
 
 function updateCurrentPos(e) {
-  // offset of the progress bar / video wrapper width
   const { left, width } = progress.getBoundingClientRect();
-  var newProgress = (e.clientX - left) / width;
+  const newProgress = (e.clientX - left) / width;
   progressBar.style.flexBasis = Math.floor(newProgress * 1000) / 10 + '%';
   video.currentTime = newProgress * video.duration;
   startVideo();
+}
+
+function updateVol(v) {
+  if (v > 0) {
+    volBtn.classList.remove('muted');
+  } else {
+    volBtn.classList.add('muted');
+  }
+  volumeBar.style.width = `${v * 100}%`;
+  volumeThumb.style.left = `calc(${v * 100}% - 7px)`;
+  video.volume = v;
 }
