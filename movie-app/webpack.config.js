66519d18ceb797e2e1181b0c937ad8d5726916a0
Yura Ya <yura.ya.dev@gmail.com>
2022-01-28 16:01:51 +0300

    init: API web-application

---
+++ movie-app/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: false,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src/assets/img'),
+    //       to: path.resolve(__dirname, 'dist/assets/img'),
+    //     },
+    //   ],
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: {} },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
