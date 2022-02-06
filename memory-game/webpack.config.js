89d4459eecf4d777903a3fbc09a82f0614f7a5d1
Yura Ya <yura.ya.dev@gmail.com>
2022-02-06 12:03:19 +0300

    refactor: readable webpack output script files in dev mode

--- memory-game/webpack.config.js
+++ memory-game/webpack.config.js
@@ -3,16 +3,27 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
+  devtool: isProd ? false : 'source-map',
   entry: {
     main: './src/index.js',
     result: './src/scripts/results.js',
   },
   output: {
-    filename: '[name].[hash].js',
+    filename: '[name].[fullhash].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
   resolve: {
     alias: {
@@ -29,7 +40,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
+      filename: '[name].[fullhash].css',
     }),
     new CopyPlugin({
       patterns: [
