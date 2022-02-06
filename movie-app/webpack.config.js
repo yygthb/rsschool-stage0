09cc1f67e9b3d77a9f0e9226d95545d740eaa16a
Yura Ya <yura.ya.dev@gmail.com>
2022-02-06 10:47:55 +0300

    refactor: readable output scripts in development mode

--- movie-app/webpack.config.js
+++ movie-app/webpack.config.js
@@ -3,15 +3,26 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
+  devtool: isProd ? false : 'source-map',
   entry: {
     main: './src/index.js',
   },
   output: {
-    filename: '[name].[fullhash].js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
   resolve: {
     alias: {
@@ -28,7 +39,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].[fullhash].css',
+      filename: '[name].[contenthash].css',
     }),
     new CopyPlugin({
       patterns: [
